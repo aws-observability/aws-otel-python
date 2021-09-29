75c11068e838bb6a602f8d7c1b11e8d8b5176191
github-action-benchmark <github@users.noreply.github.com>
2021-09-29 20:11:00 +0000

    add Soak Test Results - sample-app-flask-none (custombenchmark) benchmark result for 4c365a07477e4bc9f0a0794358580a1a01e8880a

--- soak-tests/per-commit-overall-results/data.js
+++ soak-tests/per-commit-overall-results/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632859862257,
+  "lastUpdate": 1632946260763,
   "repoUrl": "https://github.com/aws-observability/aws-otel-python",
   "entries": {
     "Soak Test Results - sample-app-flask-none": [
@@ -159,6 +159,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "Soak Test Average Virtual Memory Used",
             "value": 1205.0867456896551,
+            "unit": "Megabytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "(Eliseo) Nathaniel Ruiz Nowell",
+            "username": "NathanielRN",
+            "email": "enowell@amazon.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "4c365a07477e4bc9f0a0794358580a1a01e8880a",
+          "message": "Fix gh-pages link for auto Soak Test links (#34)",
+          "timestamp": "2021-09-29T03:28:34Z",
+          "url": "https://github.com/aws-observability/aws-otel-python/commit/4c365a07477e4bc9f0a0794358580a1a01e8880a"
+        },
+        "date": 1632946260347,
+        "tool": "custombenchmark",
+        "benches": [
+          {
+            "name": "Soak Test Average CPU Load",
+            "value": 70.92238095238096,
+            "unit": "Percent"
+          },
+          {
+            "name": "Soak Test Average Virtual Memory Used",
+            "value": 1207.295797413793,
             "unit": "Megabytes"
           }
         ]
